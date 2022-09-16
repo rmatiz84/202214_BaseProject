3442a7dba35a891da64b1bbe14f733a72e3598ee
rmatiz84 <r.matiz@uniandes.edu.co>
2022-09-15 21:33:26 -0500

    Persistencia

---
+++ src/aerolinea/aerolinea.entity.ts
@@ -0,0 +1,28 @@
+
+import { AeropuertoEntity } from "src/aeropuerto/aeropuerto.entity";
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class AerolineaEntity {
+
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    nombre: string;
+    
+    @Column()
+    descripcion: string;
+
+    @Column()
+    fechaFundacion: Date;
+
+    @Column()
+    paginaWeb: string;
+
+    @ManyToMany(() => AeropuertoEntity, aeropuerto => aeropuerto.aerolineas)
+    @JoinTable({name:'aerolinea_aeropuerto'})
+    aeropuertos: AeropuertoEntity[]
+
+}
